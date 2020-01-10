0039bbe94dc4ba51fc6513bd73ad99ff94a39fc7
Parth Aggarwal <frazile.com@gmail.com>
2020-01-10 14:52:28 +0530

    bugs fixed

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -17,59 +17,85 @@ class _IndexState extends State<Index> {
               fit: BoxFit.cover,
             ),
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  'FACEPEXELS',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32.0,
-                    fontFamily: 'SF Pro Display',
-                  ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .1,
                 ),
-                Text(
-                  'Beautiful Free Images',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                    fontFamily: 'SF Pro Display',
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.white,
-                  height: 56.0,
-                  minWidth: MediaQuery.of(context).size.width * .9,
-                  child: Text(
-                    'SIGN UP',
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 14.0,
-                      fontFamily: 'SF Pro Display',
-                    ),
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                      30.0,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Already have account?',
-                      style: TextStyle(
-                        color: Colors.white,
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              'FACEPEXELS',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 32.0,
+                                fontFamily: 'SF Pro Display',
+                              ),
+                            ),
+                            const SizedBox(height: 20.0),
+                            Text(
+                              'Beautiful Free Images',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                                fontFamily: 'SF Pro Display',
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        color: Colors.white,
+                      const SizedBox(height: 10.0),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            MaterialButton(
+                              onPressed: () {},
+                              color: Colors.white,
+                              height: 56.0,
+                              minWidth: MediaQuery.of(context).size.width * .9,
+                              child: Text(
+                                'SIGN UP',
+                                style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 14.0,
+                                  fontFamily: 'SF Pro Display',
+                                ),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                  30.0,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20.0),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  'Already have account?',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  'LOGIN',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
